a88e36105068d3715466c326875c11b360dd3d4e
dnztnc <dtia2024@mymail.pomona.edu>
2026-05-10 00:04:05 -0700

    implemented major progress checker

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,11 @@
 package src;
-//This is the Student Class
+// This is the Student Class
 // Each Student Object is 1 user
 
 
 //Imports
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Student implements StudentInterface<Course>{
@@ -34,10 +35,71 @@ public class Student implements StudentInterface<Course>{
         return this.geCoursesNeeded;
     }
 
-    //right now set to null because don't know if this should be GEs or how we are handling that yet
+    // right now set to null because don't know if this should be GEs or how we are handling that yet
     public ArrayList<Course> getCoursesNeeded(){
         return null;
     }
+
+    public void checkMajorProgress(){
+        String[] requiredCourses = {"51", "54", "62", "101", "105", "140", "190"};
+        HashMap<String, Boolean> coursesCompletedMap = new HashMap<>();
+        for (String majorCourse : requiredCourses) {
+            coursesCompletedMap.put(majorCourse, false);
+        }
+        int electivesCompleted = 0;
+
+        for (Course myCourse : this.coursesCompleted){
+            if (myCourse.getId().contains("CSCI")){
+               if (myCourse.getId().equals("CSCI051 PO")){
+                   coursesCompletedMap.put("51", true);
+               }
+            else if (myCourse.getId().equals("CSCI054 PO")){
+                   coursesCompletedMap.put("54", true);
+               }
+            else if (myCourse.getId().equals("CSCI062 PO")){
+                   coursesCompletedMap.put("62", true);
+               }
+            else if (myCourse.getId().equals("CSCI101 PO")){
+                   coursesCompletedMap.put("101", true);
+               }
+            else if (myCourse.getId().equals("CSCI105 PO") || myCourse.getId().equals("CSCI105 HM")){
+                   coursesCompletedMap.put("105", true);
+               }
+            else if (myCourse.getId().equals("CSCI140 PO") || myCourse.getId().equals("CSCI140 HM")){
+                   coursesCompletedMap.put("140", true);
+               }
+            else if (myCourse.getId().equals("CSCI190 PO")){
+                   coursesCompletedMap.put("190", true);
+               }
+            else if (myCourse.getId().contains("CSCI051L") || myCourse.getId().contains("CSCI054L") || myCourse.getId().contains("CSCI062L") || myCourse.getId().contains("CSCI101L") || myCourse.getId().contains("CSCI105L") || myCourse.getId().contains("CSCI140L") || myCourse.getId().contains("CSCI190L")){
+                // if it's a lab for one of the required courses, we do not count it as an elective
+            }
+            else{
+            //might wanna add lab checks and/or low division checks and/or repetition checks
+                electivesCompleted++;}
+            }
+        }
+        ArrayList<String> coursesStillNeeded = new ArrayList<String>();
+        for (String course : requiredCourses) {
+            if (!coursesCompletedMap.get(course)) {
+                coursesStillNeeded.add(course);
+            }
+        }
+        if (coursesStillNeeded.size() == 0 && electivesCompleted >= 3) {
+            System.out.println("Congratulations! You have completed all the required courses for your major.");
+        } else {
+            if (coursesStillNeeded.size() > 0) {
+                System.out.println("You still need to complete the following introductory/core courses for your major:");
+                for (String course : coursesStillNeeded) {
+                    System.out.println(course);
+                }
+            }
+            if (electivesCompleted < 3) {
+                System.out.println("You need to complete " + (3 - electivesCompleted) + " more elective courses.");
+            }
+        }
+        }
+
 
     //adds a course to the student's list
     public void addCourseCompleted(Course completed){
